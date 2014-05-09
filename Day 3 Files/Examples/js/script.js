067d53d36a10f7e004ec3c68029c71d3aaaf3c45
Cameron Scott <camscott@fullsail.edu>
2014-05-09 15:24:12 -0400

    Added Birth year
    Added js to figure out age.

--- Day 3 Files/Examples/js/script.js
+++ Day 3 Files/Examples/js/script.js
@@ -4,4 +4,12 @@ WPF 1405
 Day 3 Class Examples
  */
 
-alert("Your page is linked correctly.");
+//alert("Your page is linked correctly.");
+
+//Find out how old we are
+//Create a variable for birth year
+var birthYear = 1994;
+
+//Create a variable to hold the age
+var age = 2014 - birthYear;
+console.log(age);

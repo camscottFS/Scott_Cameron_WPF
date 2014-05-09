78ac7e9b7cdd1bfa4ece8a7da7719f169ea6d2ad
Cameron Scott <camscott@fullsail.edu>
2014-05-09 15:32:53 -0400

    Area of Triangle

--- Day 3 Files/Examples/js/script.js
+++ Day 3 Files/Examples/js/script.js
@@ -13,3 +13,14 @@ var birthYear = 1994;
 //Create a variable to hold the age
 var age = 2014 - birthYear;
 console.log(age);
+
+//Math
+// + - / *
+
+//Find the area of a triangle
+// base * height * 1/2
+var base = 4;
+var height = 5;
+
+var areaTriangle = base*height*.5;
+console.log(areaTriangle);

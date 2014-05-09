6d0a768b1e26807b52f6057f1d360f537dd524db
Cameron Scott <camscott@fullsail.edu>
2014-05-09 19:36:30 -0400

    Day 3 Lab Expressions
    Completed the assignment.

---
+++ Day3LabExpressions/js/script.js
@@ -0,0 +1,52 @@
+/*
+
+ Cameron Scott
+ WPF 1405
+ Day 3 Lab Expressions
+
+ */
+
+// Slice of Pizza Part 1
+
+var pizzaSlices = 12;
+var people = 6;
+var pizzaOrdered = 2;
+var result = (pizzaSlices / people) * pizzaOrdered;
+console.log("Each person ate "+result+" slices of pizza at the party.");
+
+// Slice of Pizza Part 2
+
+var leftOvers = 0;
+var sparkyEats = leftOvers;
+console.log("Sparky got "+sparkyEats+" slices of pizza.")
+// Poor Sparky :(
+
+// Average shopping bill
+
+var weeklyBill1 = 81.21;
+var weeklyBill2 = 74.83;
+var weeklyBill3 = 86.25;
+var weeklyBill4 = 97.30;
+var weeklyBill5 = 83.57;
+var totalSpent = weeklyBill1 + weeklyBill2 + weeklyBill3 + weeklyBill4 + weeklyBill5
+var totalAverage = (weeklyBill1 + weeklyBill2 + weeklyBill3 + weeklyBill4 + weeklyBill5)/5
+console.log("You have spent a total of "+totalSpent+" on groceries over 5 weeks. That is an average of "+totalAverage+" per week.")
+
+// Discounts
+
+var originalPrice = 10;
+var discountPercent = 50;
+var itemDescription = "Axe Bodyspray";
+var salesTax = 6.5;
+
+// Cost without tax
+var noTax = discountPercent/originalPrice;
+
+// Get the tax amount
+var tax = (salesTax/originalPrice)
+
+// Cost with tax
+var withTax = (discountPercent/originalPrice)+tax;
+
+// End result
+console.log("Your "+itemDescription+" was originally "+originalPrice+"$ but after a "+discountPercent+"% discount, it is now "+noTax+"$ without tax, and "+withTax+"$ with tax.")

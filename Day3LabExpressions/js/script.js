6640421a3f7e8cfe36dadbfd5b65d7512f5334e4
Cameron Scott <camscott@fullsail.edu>
2014-05-09 20:02:20 -0400

    Day 3 Lab Expressions
    Finalized activity.

--- Day3LabExpressions/js/script.js
+++ Day3LabExpressions/js/script.js
@@ -8,18 +8,16 @@
 
 // Slice of Pizza Part 1
 
-var pizzaSlices = 12;
+var pizzaSlices = 10;
 var people = 6;
 var pizzaOrdered = 2;
-var result = (pizzaSlices / people) * pizzaOrdered;
+var result = (pizzaSlices * pizzaOrdered) / people;
 console.log("Each person ate "+result+" slices of pizza at the party.");
 
 // Slice of Pizza Part 2
 
-var leftOvers = 0;
-var sparkyEats = leftOvers;
-console.log("Sparky got "+sparkyEats+" slices of pizza.")
-// Poor Sparky :(
+var leftOvers = (pizzaSlices * pizzaOrdered) % people;
+console.log("Sparky got "+leftOvers+" slices of pizza.");
 
 // Average shopping bill
 
@@ -28,9 +26,9 @@ var weeklyBill2 = 74.83;
 var weeklyBill3 = 86.25;
 var weeklyBill4 = 97.30;
 var weeklyBill5 = 83.57;
-var totalSpent = weeklyBill1 + weeklyBill2 + weeklyBill3 + weeklyBill4 + weeklyBill5
-var totalAverage = (weeklyBill1 + weeklyBill2 + weeklyBill3 + weeklyBill4 + weeklyBill5)/5
-console.log("You have spent a total of "+totalSpent+" on groceries over 5 weeks. That is an average of "+totalAverage+" per week.")
+var totalSpent = weeklyBill1 + weeklyBill2 + weeklyBill3 + weeklyBill4 + weeklyBill5;
+var totalAverage = (weeklyBill1 + weeklyBill2 + weeklyBill3 + weeklyBill4 + weeklyBill5)/5;
+console.log("You have spent a total of $"+totalSpent+" on groceries over 5 weeks. That is an average of $"+totalAverage+" per week.");
 
 // Discounts
 
@@ -43,10 +41,10 @@ var salesTax = 6.5;
 var noTax = discountPercent/originalPrice;
 
 // Get the tax amount
-var tax = (salesTax/originalPrice)
+var tax = (salesTax/originalPrice);
 
 // Cost with tax
 var withTax = (discountPercent/originalPrice)+tax;
 
 // End result
-console.log("Your "+itemDescription+" was originally "+originalPrice+"$ but after a "+discountPercent+"% discount, it is now "+noTax+"$ without tax, and "+withTax+"$ with tax.")
+console.log("Your "+itemDescription+" was originally $"+originalPrice+" but after a "+discountPercent+"% discount, it is now $"+noTax+" without tax, and $"+withTax+" with tax.");

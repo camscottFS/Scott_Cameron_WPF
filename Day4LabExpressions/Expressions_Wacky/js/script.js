946484fa09e203f2e3c4e71ae1a334c426839e85
Cameron Scott <camscott@fullsail.edu>
2014-05-14 16:47:22 -0400

    Finished Expressions Wacky

--- Day4LabExpressions/Expressions_Wacky/js/script.js
+++ Day4LabExpressions/Expressions_Wacky/js/script.js
@@ -1,6 +1,20 @@
 /*
 Cameron Scott
 WPF Section 1
-May 12, 2014
+May 14, 2014
 Expressions Wacky
  */
+
+// Lets combine random numbers
+
+//Prompt for first number
+var first = prompt("Enter a number:");
+var second = prompt("Enter a second number:");
+var third = prompt("Enter a third number:");
+var firstString = first;
+var secondString = second;
+var thirdString = third;
+
+var complete = String(firstString)+String(secondString)+String(thirdString);
+console.log(complete);
+alert("The three numbers combined are: "+complete+".");

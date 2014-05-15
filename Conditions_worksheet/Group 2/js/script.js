7f72c059724d069fa06992e9f309b4895133178e
Cameron Scott <camscott@fullsail.edu>
2014-05-14 20:06:30 -0400

    Continuing Group 3

--- Conditions_worksheet/Group 2/js/script.js
+++ Conditions_worksheet/Group 2/js/script.js
@@ -7,31 +7,11 @@ Conditions Worksheet
 
 var grade = prompt("Please enter your grade:");
 
-if(grade > 95) {
+if(grade >= 95) {
     console.log(+grade + " is an A+");
 }else{
-}
-if(grade < 95){
-    console.log(+grade+" is an A");
-}else{
-    if(grade > 85){
-        console.log(+grade+" is an B+");
-    }else{
-        if(grade < 85){
-            console.log(+grade+" is an B");
-        }else{
-            if(grade > 75){
-                console.log(+grade+" is an C+");
-            }else{
-                if(grade < 75){
-                    console.log(+grade+" is an C");
-                }else{
-                    if(grade < 70){
-                        console.log(+grade+" is an F");
-                    }else{
-                    }
-                }
-            }
-        }
+    if(grade <=94){
+    console.log(+grade +" is an A");
     }
-}
+    if(grade <=90){
+}}

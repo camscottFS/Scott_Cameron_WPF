606b0ff4046664d6dfdbca09e7ad83ba456ef6f5
Cameron Scott <camscott@fullsail.edu>
2014-07-11 19:35:42 -0400

    fun with storage boxes

--- Day 3 Files/js/script.js
+++ Day 3 Files/js/script.js
@@ -6,3 +6,15 @@ Day 3 First File
  */
 
 alert("Hello World");
+
+console.log("This is a console statement.");
+
+var whatever1; // Declaring this bitch
+whatever1 = 42;  // This is a definition
+
+var a = 2;
+
+console.log(a);
+
+a = a + 3;
+console.log(a);

065c1ea65d32d4f2721639ac82552b2f3a19afea
Cameron Scott <camscott@fullsail.edu>
2014-07-11 20:03:54 -0400

    Commit this Stuff

--- Day 3 Files/js/script.js
+++ Day 3 Files/js/script.js
@@ -18,3 +18,22 @@ console.log(a);
 
 a = a + 3;
 console.log(a);
+
+var yearBorn = 1994;
+
+var age = 2014-yearBorn;
+
+console.log(age);
+
+var base = 4;
+var height = 5;
+
+var areaOfTriangle = base * height / 2;
+console.log(areaOfTriangle);
+
+var remainder = 32%10;
+
+console.log(remainder);
+
+var evenOrOdd = 67%2;
+console.log(evenOrOdd);
